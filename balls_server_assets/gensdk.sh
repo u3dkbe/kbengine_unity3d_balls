f99362faf0ff525921d585b46eb77d99f62ab953
kebiao <380000937@qq.com>
2019-02-21 11:23:56 +0800

    up

--- balls_server_assets/gensdk.sh
+++ balls_server_assets/gensdk.sh
@@ -13,18 +13,12 @@ else
 	export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
 fi
 
-
-
 export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$(pwd):$(pwd)/res:$(pwd)/scripts/"
 export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
 
-userid=`id -u $1 &>/dev/null`
-XUID=$?
+echo KBE_ROOT = \"${KBE_ROOT}\"
+echo KBE_RES_PATH = \"${KBE_RES_PATH}\"
+echo KBE_BIN_PATH = \"${KBE_BIN_PATH}\"
 
-if [ $XUID = 0 ]
-then
-	XUID=`$KBE_BIN_PATH/kbcmd --getuid`
-	echo UID=$XUID
-fi
-
-python $KBE_ROOT/kbe/tools/server/pycluster/cluster_controller.py stop $XUID
+$KBE_BIN_PATH/kbcmd --clientsdk=unity --outpath=$currPath/kbengine_unity3d_plugins
+$KBE_BIN_PATH/kbcmd --clientsdk=ue4 --outpath=$currPath/kbengine_ue4_plugins
